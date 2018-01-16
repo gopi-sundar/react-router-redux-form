44cec33296047eccd25be348c8b04df15de9c78e
Sundararaju, Gopinath <gopinath.sundararaju@pearson.com>
2018-01-16 17:56:32 -0600

    2 list of posts in route /

---
+++ src/reducers/reducer_posts.js
@@ -0,0 +1,13 @@
+import _ from "lodash";
+
+import {FETCH_POSTS} from '../actions/index';
+
+export default function(state = {},action) {
+    switch(action.type) {
+        case FETCH_POSTS:
+            // convert array to key value object
+            return _.mapKeys(action.payload.data,'id');
+        default:
+            return state;
+    }
+}
