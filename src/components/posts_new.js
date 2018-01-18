ef6cf925228292a64d0293ba6903a395ee487d26
Sundararaju, Gopinath <gopinath.sundararaju@pearson.com>
2018-01-18 16:20:30 -0600

    3 using redux-form formReducer form field validation

---
+++ src/components/posts_new.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Field, reduxForm } from 'redux-form';
+
+class PostsNew extends Component {
+    renderField(field) {
+        return (
+            <div className="form-group">
+                <label>{field.label}</label>
+                <input className="form-control" type="text" {...field.input} />
+                {field.meta.error}
+            </div>
+        );
+    }
+    render() {
+        return (
+            <div>
+                <form>
+                    <Field label="Title" name="title" component={this.renderField} />
+                    <Field label="Categories" name="categories" component={this.renderField} />
+                    <Field label="Post Content" name="content" component={this.renderField} />
+                </form>
+                PostsNew!
+            </div>
+        )
+    }
+}
+
+function validate(values) {
+    //console.log(values) -> {title: 'asdf', categories : 'wetwet' , content:'wrdw'}
+    const errors = {};
+
+    // Validate the inputs from 'values'
+    if (!values.title || values.title.length < 3) {
+        errors.title = 'Enter a title that is atleast 3 character!'
+    }
+    if (!values.categories) {
+        errors.categories = 'Enter some categories!'
+    }
+    if (!values.content) {
+        errors.content = 'Enter post content!'
+    }
+
+    // If empty, validation is successful
+    // If any  properties inside, redux form assumes form is invalid
+    return errors;
+}
+
+export default reduxForm({
+    validate,
+    form: 'PostsNewForm'
+})(PostsNew);
